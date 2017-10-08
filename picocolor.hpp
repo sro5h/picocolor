1c743438b4761693482824a9a5eed7fe88359b69
Paul Meffle <meffle.paul@me.com>
2017-10-08 15:01:26 +0200

    Increment version number

--- picocolor.hpp
+++ picocolor.hpp
@@ -1,5 +1,5 @@
 /*
-   picocolor.hpp - 1.0.1
+   picocolor.hpp - 1.1.0
 
    Author:
         Paul Meffle
@@ -11,6 +11,7 @@
    Revision history:
         1.0   (14.08.2017) initial release
         1.0.1 (13.09.2017) add documentation
+        1.1.0 (08.10.2017) only output color sequence if std::ostream is a tty
 */
 
 #ifndef _PICOCOLOR_HPP
